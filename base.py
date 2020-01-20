4c48f7d0dfeeddef7850b1ac0e30bc22a467c10c
lpverneck <lpverneck@gmail.com>
2020-01-20 11:08:28 -0300

    results save fixed

--- base.py
+++ base.py
@@ -1,5 +1,5 @@
 import os
-import json
+import pickle
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -35,22 +35,19 @@ for station in files:
     df['month'] = df.index.month
     df['weekday name'] = df.index.weekday_name
 
-
     # Data split
     X_train, X_test = df.loc['1998':'2008'], df.loc['2009':'2012']
     y_train, y_test = X_train['solar radiation'], X_test['solar radiation']
-    X_train = X_train.drop(['year', 'month', 'weekday name', 'solar radiation'],
-                            axis=1)
+    X_train = X_train.drop(['year', 'month', 'weekday name', 'solar radiation']
+                           , axis=1)
     X_test = X_test.drop(['year', 'month', 'weekday name', 'solar radiation'],
-                          axis=1)
-
+                         axis=1)
 
     # Pipeline creation
     pipeline = Pipeline([
         ('poly_features', PolynomialFeatures()),
         ('lr_reg', LinearRegression())
     ])
-
 
     # Values for GridSearchCV to iterate over
     param_grid = {
@@ -61,28 +58,23 @@ for station in files:
         'lr_reg__normalize': [True, False]
     }
 
-
     # Scores
     scoring = ['neg_mean_squared_error', 'neg_mean_absolute_error', 'r2']
 
-
     # Do 10 CV for each of the 6 possible combinations of the parameter values
-    grid = GridSearchCV(pipeline, cv=10, param_grid=param_grid, scoring=scoring,
-                        refit='r2', iid=False, return_train_score=True)
+    grid = GridSearchCV(pipeline, cv=10, param_grid=param_grid, refit='r2',
+                        scoring=scoring, iid=False, return_train_score=True)
     grid.fit(X_train, y_train)
-
 
     # Summarize results
     pd.DataFrame(grid.cv_results_)
-    print("Best Estimator:", grid.best_estimator_)  # Best estimator config
-    print("Best Score:", grid.best_score_)  # Mean CV score of the best_estimator
+    print("Best Estimator:", grid.best_estimator_)
+    print("Best Score:", grid.best_score_)
     print("Best Params:", grid.best_params_)
     print("Best Index:", grid.best_index_)
 
-
     # Predict test instances using the best configs found in the CV step
     y_pred = grid.predict(X_test)
-
 
     # Metric calculations
     print("MSE:", metrics.mean_squared_error(y_test, y_pred, squared=True))
@@ -97,10 +89,19 @@ for station in files:
     attr.append(grid.best_index_)
     attr.append(grid.best_estimator_)
     attr.append(pd.DataFrame(grid.cv_results_))
-
+    attr.append(metrics.mean_squared_error(y_test, y_pred, squared=True))
+    attr.append(metrics.mean_squared_error(y_test, y_pred, squared=False))
+    attr.append(metrics.mean_absolute_error(y_test, y_pred))
+    attr.append(metrics.r2_score(y_test, y_pred))
 
     fold.append(attr)
 
 
-with open('results.txt', 'w') as f:
-    f.write(json.dumps(fold))
+# Save the simulation results into a pickle file
+with open('results.pickle', 'wb') as f:
+    pickle.dump(fold, f)
+
+
+# Load the results saved
+# with open('results.pickle', 'rb') as f:
+#     results = pickle.load(f)
