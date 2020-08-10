d17aec2646d1bd3e4d7bb7ce21cf1941d22cb57d
lpverneck <lpverneck@gmail.com>
2020-08-10 15:39:36 -0300

    src/model_rebuild_DRAFT.py formatting

--- src/model_rebuild_DRAFT.py
+++ src/model_rebuild_DRAFT.py
@@ -1,4 +1,4 @@
-"""Model Rebuild
+"""Model Rebuild - DRAFT
 
 Loads a specific previously trained model to access his attributes and
 to predict new data.
@@ -6,9 +6,9 @@ to predict new data.
 
 
 import os
+import joblib
 import numpy as np
 import pandas as pd
-from sklearn.externals import joblib
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import Ridge
 from sklearn.model_selection import KFold
@@ -18,11 +18,12 @@ from sklearn.preprocessing import PolynomialFeatures
 
 
 # Take stations name
-data_dir = os.getcwd()[:-6] + "\\data\\raw"
+data_dir = os.getcwd()[:-4] + "/data/raw"
 files = os.listdir(data_dir)
+files.sort()
 files.pop(0)
 
-# Choose the run number and station
+# Choose the run number and station model
 run = 1
 station = files[0][:-5]
 
@@ -36,10 +37,10 @@ loaded_model = joblib.load(station + "_" + str(run) + "_model.pkl")
 y_p = loaded_model.predict()
 
 # Attributes
-len(loaded_model.named_steps['ridge_reg'].coef_)
-loaded_model.named_steps['ridge_reg'].coef_
-loaded_model.named_steps['poly_features'].n_input_features_
-loaded_model.named_steps['poly_features'].n_output_features_
-loaded_model.named_steps['features_select'].scores_
-len(loaded_model.named_steps['features_select'].scores_)
-len(loaded_model.named_steps['features_select'].pvalues_)
+len(loaded_model.named_steps["ridge_reg"].coef_)
+loaded_model.named_steps["ridge_reg"].coef_
+loaded_model.named_steps["poly_features"].n_input_features_
+loaded_model.named_steps["poly_features"].n_output_features_
+loaded_model.named_steps["features_select"].scores_
+len(loaded_model.named_steps["features_select"].scores_)
+len(loaded_model.named_steps["features_select"].pvalues_)
